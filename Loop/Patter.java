3b8cb8e871f0c564cd5ae251919fc79d5deb1096
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-07-13 19:46:14 +0530

    Create Patter.java

---
+++ Loop/Patter.java
@@ -0,0 +1,41 @@
+public class Patter {
+    public static void main(String[] args) {
+
+        // Question 1
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" ");
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+        }
+
+        // Question 2
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+        }
+
+        // Question 3
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+        }
+
+        // Question 4
+        char letter = 'A';
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(" ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print(letter);
+                letter++;
+            }
+        }
+
+        // Question 5
+        
+    }
+}
