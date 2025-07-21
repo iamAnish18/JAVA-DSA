e62205360639b645f841211bd8fba29a08001b05
Anish Tamoli <iamanishtamoli18@gmail.com>
2025-07-21 23:55:32 +0530

    Update AdvancedPatter.java

--- Loop/AdvancedPatter.java
+++ Loop/AdvancedPatter.java
@@ -47,11 +47,25 @@ public class AdvancedPatter {
         }
     }
 
+    public static void ZeroOneTriangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i == j || i - j == 2 || i -j ==4) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println(" ");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         HollowRectangle(4, 5);
         HalfPyramid(4);
         HalfPyramidNumber();
         FlodTriangle();
+        ZeroOneTriangle(5);
     }
 }
